ce9cc1590ee8e8f354c931c4aaff027c26da2857
Elcio de Souza Monico <elcio.monico@gmail.com>
2026-07-26 18:27:25 -0300

    feat: implement HttpInputFactory for request processing and BaseApiController for standardized API responses

--- App/Factories/HttpInputFactory.php
+++ App/Factories/HttpInputFactory.php
@@ -124,7 +124,7 @@ final class HttpInputFactory extends BaseFactory
             'months'           => (string) $input->months,
             'capital'          => $input->initialCapital,
             'cdi'              => $input->cdiPercentage,
-            'selic_meta'       => $input->selicMetaDefault !== '' ? $input->selicMetaDefault : $input->selicMeta,
+            'selic_meta'       => $input->selicMeta !== '' ? $input->selicMeta : ($input->selicMetaDefault !== '' ? $input->selicMetaDefault : ''),
             'selic_meta_default' => $input->selicMetaDefault,
             'pre_rate'         => $input->preFixedAnnualRate,
             'cdi_annual'       => $input->cdiOver,
